1bc820863c3dcb99a36efd9fccf878c67ab28f19
Chuy <chuuuuy@gmail.com>
2015-03-17 15:43:38 -0400

    Update this4that.py

--- this4that.py
+++ this4that.py
@@ -3,14 +3,20 @@ import re
 import os
 from sys import argv
 
-"""A Script for changing names of OTUS in phylogenetic files (or any other text file). In case of fasta files, it replaces the whole identifier line with the new name. For newick, nexus or other files.. it only changes matching names.
-Replacing names can be easily accomplished using sed, but it might result complicated when renaming hundreds of leaves in many files this script is intended to facilitate that task, with little editing. You can create your reference CSV in any spreadsheet and save it as a comma delited text file.
+"""A Script for changing names of OTUS in phylogenetic files (or any other text file). In case of fasta files, it replaces the whole identifier line with the new name. For newick, nexus or other files it only changes matching names.
+Replacing names can be easily accomplished using sed or regex, but it might result complicated when renaming hundreds of leaves in many files this script is intended to facilitate that task, with little editing. You can create your reference CSV in any spreadsheet and save it as a comma delited text file.
+
+This script does not replace the input file, instead creates a version of the same file with 'v2' in its name. 
 
 
-
-TODO multiculumn csv  and more complex name editor.
-
-Usage: python this4that.py names.csv target.fasta"""
+TODO 
+    * multiculumn csv and more complex name editor˘
+    * verify all identifiers where replaced or write an alert.
+Usage: 
+    python this4that.py names.csv target.fasta
+    or
+    pythin this4that.py names.csv *.faa
+    """
 
 CSV_file = argv[1]
 argv.remove(argv[0])
@@ -53,8 +59,9 @@ for File in Target:
                 Out.write(Line)
         Out.close()
         if Changes == 0:
+            Log.write('%s: %d changes.' %(F, Changes))
             os.remove(OutFName)
         else:
-            Log.write('%d changes were made in %s' %(Changes, F))
+            Log.write('%s: %d changes.' %(F, Changes))
         
 Log.close()
