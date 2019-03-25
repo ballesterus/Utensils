b9a720cc0e997ac95b445e6e4d38e14ed1cea081
ballesterus <chuuuuy@gmail.com>
2019-03-25 16:28:35 -0500

    improved (?) replacement... more testing

--- this4that.py
+++ this4that.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import re
 import os
+#import UPhO
 from sys import argv
+
 
 """A Script for changing names of OTUS in phylogenetic files (or any other text file). In case of fasta files, it replaces the whole identifier line with the new name. For newick, nexus or other files it only changes matching names.
 Replacing names can be easily accomplished using sed or regex, but it might result complicated when renaming hundreds of leaves in many files this script is intended to facilitate that task, with little editing. You can create your reference CSV in any spreadsheet and save it as a comma delimited text file.
@@ -18,55 +20,52 @@ Usage:
     python this4that.py names.csv *.faa
     """
 
+
+
 CSV_file = argv[1]
 argv.remove(argv[0])
 argv.remove(CSV_file)
-print argv
+#print argv
 Target = argv
 Log = open('this4that.log', 'w')
-Map={}
+
+def makefromtodict(namesCSV):
+    namesdict={}
+    with open(namesCSV, 'r') as F: 
+        for Line in F:
+            try:
+                From, To = Line.replace(', ', '').split(',')
+                To = To.strip('\n')
+                namesdict[From] = To
+            except:
+                print "ERROR on: %s" % (Line)
+    return namesdict
 
 
+###MAIN###
 
-with open(CSV_file, 'r') as F: 
-    for Line in F:
-        try:
-            From, To = Line.replace(', ', '').split(',')
-            To = To.strip('\n')
-            Map[From] = To
-        except:
-            print "ERROR on: %s" % (Line)
-#print Map
 
+FTdict=makefromtodict(CSV_file)
 for File in Target:
     with open(File, 'r') as F:
         Changes =0
-        OutFName = '%s_v2.%s' % (File.split('.')[0], File.split('.')[1])
+        OutFName = '%s_renamed.%s' % (File.split('.')[0], File.split('.')[1])
         Out= open(OutFName,'w')
         for Line in F:
-            if Line.startswith('>'):
-                Matches =0
-                for From in Map.iterkeys():
-                    if re.search(From, Line):
-                        NL = Map[From]
-                        Out.write('>%s\n' % NL)
-                        Changes +=1
-                        Matches+=1
-                if Matches == 0:
-                    Out.write('%s' % Line)
-                    print 'ALERT: The identifier %s was not found in the names catalog!' % Line
-
-            else:
-                for From in Map.iterkeys():
-                    if re.search(From, Line):
-                        Line = re.sub(From, Map[From], Line)
-                        Changes+=1
-                Out.write(Line)
+            for From in FTdict.iterkeys():
+                 To=FTdict[From]
+                 reexpress="(?<![0-9A-Za-z_:|\.])%s(?=[,;\):\n\s])" % From
+                 if re.search(reexpress, Line):
+                     Line=re.sub(reexpress, To, Line)
+                     Changes +=1
+            Out.write(Line)
         Out.close()
-        if Changes == 0:
-            Log.write('%s: %d changes.' %(F, Changes))
+        print "Names found/changed in %s = %d\n" %(File,Changes)
+        if Changes == 0: 
+            Log.write('%s: %d changes.' %(File, Changes))
             os.remove(OutFName)
         else:
-            Log.write('%s: %d changes.' %(F, Changes))
+            Log.write('%s: %d changes.' %(File, Changes))
         
-Log.close()
+        Log.close()
+print "All done!"
